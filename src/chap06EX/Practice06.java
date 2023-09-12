5a0ff5c91e8a72dc67cecdcf96939889afefe28c
N1C0N1C0 <wech7883@naver.com>
2023-09-12 20:44:54 +0900

    insertionSort add

--- src/chap06EX/Practice06.java
+++ src/chap06EX/Practice06.java
@@ -1,15 +1,15 @@
-package chap06;
+package chap06EX;
 
 import java.util.Scanner;
 
-public class InsertionSort {
+public class Practice06 {
     static void insertionSort(int[] a, int n) {
         for(int i=1; i<n; i++) {
             int j;
-            int tmp = a[i];
-            for(j=i; j>0 && a[j-1] > tmp; j--)
+            int temp = a[i];
+            for(j=i; j>0 && a[j-1] > temp; j--)
                 a[j] = a[j-1];
-            a[j] = tmp;
+            a[j] = temp;
         }
     }
 
